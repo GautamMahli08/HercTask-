77f824411048b1112f77699b54a7615d76d28518
Gautam Mahli <140971352+GautamMahli08@users.noreply.github.com>
2025-02-15 08:08:01 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -14,7 +14,7 @@ const Login = ({ setToken }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://herctask.onrender.com", {
         username,
         password,
       });
